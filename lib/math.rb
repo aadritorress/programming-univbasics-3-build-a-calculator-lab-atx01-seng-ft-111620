7763c56c7882d668242c4edcd0f80df2dd483d8e
Adri Torres <adriana.amaral@msn.com>
2020-10-24 04:19:00 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,15 +1,15 @@
 def addition(num1, num2)
-return num2 + num1 
+return num1 + num2 
 end
 addition("4", "5")
 
 def subtraction(num1, num2)
-return num2 - num1
+return num1 - num2
 end
 subtraction("5", "10")
 
 def division(num1, num2)
-return num2 / num1
+return num1 / num2
 end
 division("4", "5")
 
