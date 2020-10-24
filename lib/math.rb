0bb927478a3d86907dca31d5b3450b625eaed844
Adri Torres <adriana.amaral@msn.com>
2020-10-24 04:15:50 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,18 +1,22 @@
 def addition(num1, num2)
-
+return num2 + num1 
 end
+addition("4", "5")
 
 def subtraction(num1, num2)
-
+return num2 - num1
 end
+subtraction("5", "10")
 
 def division(num1, num2)
-
+return num2 / num1
 end
+division("4", "5")
 
 def multiplication(num1, num2)
-
+return num1 * num2
 end
+multiplication("4", "5")
 
 def modulo(num1, num2)
 
